30ef70110564e9a22c25b51237f5c9ec36db31ba
Mayowa Aj <sagaraaj69@gmail.com>
2019-12-31 13:25:02 +0100

    can read rom data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 
 fn main() {
     println!("Hello, this is my chip 8 emulator!");
+
+    let args: Vec<String> = env::args().collect();
+    let file_name = match args.len() {
+        0 | 1 => {
+          panic!("invalid argument length");  
+        },
+        _ => args.get(1).unwrap(),
+    };
+    let mut file = File::open(file_name).unwrap();
+    let mut data = Vec::<u8>::new();
+    file.read_to_end(&mut data).expect("File not found!");
+
+    
 }
