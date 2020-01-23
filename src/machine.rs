64625da0652ca31fb1cca94c4f9917bc75565114
Mayowa Aj <sagaraaj69@gmail.com>
2020-01-23 11:44:53 +0100

    WIP: ggez for event loop and display

--- src/machine.rs
+++ src/machine.rs
@@ -2,6 +2,9 @@ use crate::cpu::Cpu;
 use crate::bus::Bus;
 use crate::keyboard::Keyboard;
 use crate::display::Display;
+use ggez::{Context, ContextBuilder, GameResult};
+use ggez::event::{self, EventHandler};
+use ggez::graphics;
 
 
 //#[derive(Debug)]
@@ -27,17 +30,37 @@ impl Machine {
     }
 
     pub fn start(&mut self) { //TODO return result
-        loop {
+         
 
-            //TODO check for keyboard input
-            if self.cpu.should_execute {
-                self.cpu.execute_instruction(&mut self.bus);
+            let (mut ctx, mut event_loop) = ContextBuilder::new("game_name", "author_name")
+                                            .build()
+                                            .unwrap();
+
+   
+
+            match event::run(&mut ctx, &mut event_loop, self) {
+                Ok(_) => println!("Exited cleanly."),
+                Err(e) => println!("Error occured: {}", e)
             }
 
-            //on keypress event, check if cpu is waiting for keypress
-            //if it is set the last key pressed, if not do nothing
             
-            
+        
+    }
+}
+
+impl EventHandler for Machine {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+       if self.cpu.should_execute {
+            self.cpu.execute_instruction(&mut self.bus);
         }
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+
+        // Draw code here...
+
+        graphics::present(ctx)
     }
 }
